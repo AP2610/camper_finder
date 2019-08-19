7198aab717601457a1bfd4c52fc0f6ae1543e2e6
Arjun Puri <arjun_261094@hotmail.com>
2019-08-19 18:47:37 +0200

    SETUP_Added profile route to routes.rb, added profile action to pages controller, added if logic to profile action, added skip_before_action to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+  # skip_before_action :authenticate_user!, only: :home
   def home
+  end
+
+  def profile
+    if current_user.present?
+      render :profile
+    else
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'profile', to: 'pages#profile', as: :profile
 
   resources :van do
     resources :booking, only: [:index, :new, :create, :destroy]
