5cf1c027ace1d700520fe2d1d3c8b296209b395e
Arjun Puri <arjun_261094@hotmail.com>
2019-08-19 17:36:25 +0200

    Added Review Model and Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_153425) do
+ActiveRecord::Schema.define(version: 2019_08_19_153549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2019_08_19_153425) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["van_id"], name: "index_bookings_on_van_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.bigint "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,5 +69,6 @@ ActiveRecord::Schema.define(version: 2019_08_19_153425) do
 
   add_foreign_key "bookings", "users"
   add_foreign_key "bookings", "vans"
+  add_foreign_key "reviews", "bookings"
   add_foreign_key "vans", "users"
 end
