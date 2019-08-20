6645b375023fc96bd579862bd4338831058d6e47
Margarita <l.m.klingen@gmail.com>
2019-08-20 12:22:35 +0200

    FIX_seeds_file_van_title_2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
@@ -31,7 +32,7 @@ puts "Now we have #{User.count} users"
 
 25.times do
   van = Van.create(
-    title: "#{Faker::Coffee.blend_name} Van"),
+    title: "#{Faker::Coffee.blend_name} Van",
     description: Faker::Lorem.paragraph(sentence_count: 4),
     address: Faker::Address.full_address,
     van_model: Faker::Vehicle.make,
