3db2a16163643f0ba921b36e57076db8e07ee547
Arjun Puri <arjun_261094@hotmail.com>
2019-08-21 15:34:46 +0200

    Added configuration for photo uploader so that all profile pictures have the version 'avatar', also added ability to show profile picture on profile page, also added version for navbar image avatar so that it only takes the face

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,21 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  process eager: true # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  process resize_to_fit: [300, 300]
+
+  version :avatar do
+    cloudinary_transformation transformation: [
+      { width: 400, height: 400, gravity: :face, radius: :max, crop: :thumb },
+      { width: 200, height: 200, crop: :thumb }
+    ]
+  end
+
+  version :bright_face do
+    cloudinary_transformation radius: 20,
+      width: 150, height: 150, crop: :thumb, gravity: :face
+  end
 end
