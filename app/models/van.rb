766a75947b277d426f062d1a25dfce7c9b8ca559
Christina Meliniotou <chris.melinioti@gmail.com>
2019-08-21 15:18:56 +0200

    fixed the margins of the forms

--- app/models/van.rb
+++ app/models/van.rb
@@ -4,7 +4,6 @@ class Van < ApplicationRecord
   has_many :reviews, through: :bookings, dependent: :destroy
 
   validates :title, :address, :price_cents, presence: true
-  validates :pax, inclusion: { in: [1, 2, 3, 4] }
 
   mount_uploader :photo, PhotoUploader
 end
